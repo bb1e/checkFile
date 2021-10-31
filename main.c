1c34fed03c220c8309d47fecfcfa83e42d11c751
Barbie Chan <63793516+bbarbie@users.noreply.github.com>
2021-10-31 18:03:33 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -18,20 +18,54 @@
 #include <assert.h>
 #include <stdint.h>
 #include <dirent.h>
+#include <signal.h>
+#include <ctype.h>
+#include <time.h>
+#include <libgen.h>
 
 #include "debug.h"
 #include "memory.h"
 #include "args.h"
 
 
+#define MAX 100
+
+void trata_sinal(int signal);
 void help();
 
 
 int main(int argc, char *argv[]) {
 
+    char buf[MAX];
+    struct sigaction act;
+    ssize_t n;
+
     (void)argc;
     (void)argv; 
 
+    act.sa_handler = trata_sinal;
+
+    sigemptyset(&act.sa_mask);
+
+    act.sa_flags = SA_RESTART;
+
+
+    if (sigaction(SIGUSR1, &act, NULL) < 0)
+    {
+        ERROR(1, "sigaction - SIGUSR1");
+    }
+
+    printf("PID do processo: %d\n", getpid());
+    printf("Mata o processo!!!:\n");
+
+    n = read(0, buf, MAX);
+    if (n < 0){
+        ERROR(2, "Erro de entrada.");
+    buf[n] = '\0';
+    }
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        printf("%d.%02d.%02d_%02dh%02d:%02d  --- %d\%c", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,getpid(), argc);
 
     struct gengetopt_args_info args;
 	if (cmdline_parser(argc,argv,&args) != 0){
@@ -166,6 +200,14 @@ void help() {
     printf("Author 1: Bruna Bastos LeaL -- 2201732\n");
     printf("Author 2: Tomás Vilhena Pereira -- 2201785\n");
     printf("File extensions compatible with the program: \nPDF, GIF, JPG, PNG, MP4, ZIP, HTML\n");
-    exit(1);
+    exit(0);
 }
 
+
+void trata_sinal(int signal) {
+    int aux;
+    aux = errno;
+
+    /* Restaura valor da variavel global errno */
+    errno = aux;
+}
